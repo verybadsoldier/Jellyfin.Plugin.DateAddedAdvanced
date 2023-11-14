ed5e44d9c4c0dfb3468bf8706abb4247c6692862
verybadsoldier <vbs@springrts.de>
2023-11-14 21:19:20 +0100

    added nfo plugin

---
+++ Jellyfin.Plugin.NfoDateCreated/Configuration/PluginConfiguration.cs
@@ -0,0 +1,35 @@
+using MediaBrowser.Model.Plugins;
+
+namespace MediaBrowser.Providers.Plugins.NfoCreateDate.Configuration
+{
+    /// <summary>
+    /// Plugin configuration class for the studio image provider.
+    /// </summary>
+    public class PluginConfiguration : BasePluginConfiguration
+    {
+        private string _repository = Plugin.DefaultServer;
+
+        /// <summary>
+        /// Gets or sets the studio image repository URL.
+        /// </summary>
+        public string RepositoryUrl
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(_repository))
+                {
+                    _repository = Plugin.DefaultServer;
+                }
+
+                return _repository;
+            }
+
+            set
+            {
+                _repository = string.IsNullOrEmpty(value)
+                    ? Plugin.DefaultServer
+                    : value.TrimEnd('/');
+            }
+        }
+    }
+}
